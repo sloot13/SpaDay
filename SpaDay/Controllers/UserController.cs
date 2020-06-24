be13752c2e8964801b50ff0d644533217e9aad72
Kevin Franklin <deerdance72@gmail.com>
2020-06-24 15:19:51 -0400

    test

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SpaDay.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("user/add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if( newUser.Password == verify)
+            {
+                ViewBag.userName = newUser.Username;
+                return View("Index");
+            }
+            
+            return View();
+        }
+    }
+}
